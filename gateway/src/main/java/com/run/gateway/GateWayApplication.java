85fec3da0b28c30a31e532cb7198ba8bc5d76482
mujian <m_jevon@163.com>
2020-01-07 18:01:10 +0800

    feat: 引用nacos

--- gateway/src/main/java/com/run/gateway/GateWayApplication.java
+++ gateway/src/main/java/com/run/gateway/GateWayApplication.java
@@ -3,14 +3,12 @@ package com.run.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
 public class GateWayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GateWayApplication.class,args);
+        SpringApplication.run(GateWayApplication.class, args);
     }
 }
---
+++ register/src/test/java/cn/leon/register/RegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.leon.register;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
